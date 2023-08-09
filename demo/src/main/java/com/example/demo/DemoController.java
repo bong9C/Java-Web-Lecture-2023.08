8474e7ac5e072ce92c187b4761fcbeb693fb0ff4
SeHee JU <kathy1221@naver.com>
2023-08-09 17:30:19 +0900

    2023.08.09 Spring&HTML

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.Stack;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,17 +9,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class DemoController {
+	String a_ = null, b_ = null, op = null;
+	int a = 0, b = 0, result = 0;
 	
+	// localhost:8080/demo/index
 	@GetMapping("/index")
 	@ResponseBody
 	public String index() {
-		return "<h1> Hello word!!!</h1>";
+		return "<h1>Hello World!!!</h1>";
 	}
-		
-	//localhost:8090/demo/hello
+	
 	@GetMapping("/hello")
 	public String hello() {
 		return "01.hello";
@@ -28,13 +33,12 @@ public class DemoController {
 		return "02.login";
 	}
 	
-	@PostMapping("/login")
-	//@ResponseBody
+	@PostMapping("/login") 
+//	@ResponseBody
 	public String loginProc(HttpServletRequest req, Model model) {
-
-		String uid = req.getParameter("uid");			// "uid" 가 input tag 의 name값
+		String uid = req.getParameter("uid");		// "uid" 가 input tag의 name 값
 		String pwd = req.getParameter("pwd");
-		//return "<h1>uid=" + uid + ", pwd=" + pwd + "</h1>";
+//		return "<h1>uid=" + uid + ", pwd=" + pwd + "</h1>";
 		
 		model.addAttribute("uid", uid);
 		model.addAttribute("pwd", pwd);
@@ -53,27 +57,25 @@ public class DemoController {
 		String oper = "";
 		switch(op) {
 		case "add":
-			result = a+b; oper = "+";
+			result = a + b; oper = "+";
 			break;
-		case"sub":
-			result = a - b; oper = "-" ;
+		case "sub":
+			result = a - b; oper = "-";
 			break;
-		case"mul":
-			result = a * b; oper = " *";
+		case "mul":
+			result = a * b; oper = "*";
 			break;
 		case "div":
-			result = (int)(a/b); oper = "/";
+			result = (int) (a / b); oper = "/";
 			break;
 		default:
 			result = 0;
-				
 		}
-		return"<h1>" + a + oper + b + "=" + result + "</h1>";
+		return "<h1>" + a + " " + oper + " " + b + " = " + result + "</h1>";
 	}
 	
 	@GetMapping("/calc")
 	public String calcForm() {
-		
 		return "04.calcForm";
 	}
 	
@@ -83,27 +85,25 @@ public class DemoController {
 		String oper = "";
 		switch(op) {
 		case "add":
-			result = a+b; oper = "+";
+			result = a + b; oper = "+";
 			break;
-		case"sub":
-			result = a - b; oper = "-" ;
+		case "sub":
+			result = a - b; oper = "-";
 			break;
-		case"mul":
-			result = a * b; oper = " *";
+		case "mul":
+			result = a * b; oper = "*";
 			break;
 		case "div":
-			result = (int)(a/b); oper = "/";
+			result = (int) (a / b); oper = "/";
 			break;
 		default:
 			result = 0;
-				
 		}
-		model.addAttribute("a",a);
-		model.addAttribute("b",b);
-		model.addAttribute("oper",oper);
-		model.addAttribute("result",result);
+		model.addAttribute("a", a);
+		model.addAttribute("b", b);
+		model.addAttribute("oper", oper);
+		model.addAttribute("result", result);
 		return "05.calcResult";
-		
 	}
 	
 	@GetMapping("/write")
@@ -117,11 +117,64 @@ public class DemoController {
 		String[] languages = req.getParameterValues("language");
 		String content = req.getParameter("content");
 		
-		
-		String joinLanguages = (languages == null) ? "" : String.join(", ",  languages);
+		String joinLanguages = (languages == null) ? "" : String.join(", ", languages);
 		Board board = new Board(title, joinLanguages, content.replace("\n", "<br>"));
-		//System.out.println(board);
+//		System.out.println(board);
 		model.addAttribute("board", board);
 		return "07.writeResult";
 	}
+	
+	@GetMapping("/calculator")
+	public String calculatorForm(String eval, Model model) {
+		model.addAttribute("eval", eval);
+		return "08.calculator";
+	}
+	
+//		Object obj = session.getAttribute("stack");
+//		Stack<Object> stack = (obj == null) ? new Stack<>() : (Stack) obj;
+	
+	@PostMapping("/calculator")
+	public String calculatorProc(HttpServletRequest req, Model model) {
+		String num_ = req.getParameter("num");
+		String op_ = req.getParameter("op");
+		String eval = req.getParameter("eval");
+		if (eval == null)
+			eval = "";
+		
+		if (num_ != null) {
+			eval += num_;
+			if (a_ == null) {
+				a_ = num_;
+				a = Integer.parseInt(a_);
+			} else if (b_ == null) {
+				b_ = num_;
+				b = Integer.parseInt(b_);
+			}
+			model.addAttribute("eval", eval);
+		} else if (op_ != null) {
+			if (op_.equals("C")) {
+				eval = "";
+				a_ = null; b_ = null; op = null;
+				a = 0; b = 0;
+				model.addAttribute("eval", eval);
+			} else if (op_.equals("=")) {
+				switch(op) {
+				case "+": result = a + b; break;
+				case "-": result = a - b; break;
+				case "*": result = a * b; break;
+				case "/": result = (int) (a / b); break;
+				default: result = 0;
+				}
+				a_ = null; b_ = null; op = null;
+				a = 0; b = 0;
+				model.addAttribute("eval", result);
+			} else {
+				eval += " " + op_ + " ";
+				op = op_;
+				model.addAttribute("eval", eval);
+			}
+		}
+		return "08.calculator";
+	}
+	
 }
