0350e82666c5160f434bcd384dc16e54012e0371
SeHee JU <kathy1221@naver.com>
2023-08-28 17:37:59 +0900

    2023.08.28 중국 드라마 블로그 만들기

--- demo/src/main/java/com/example/demo/chinadrama/Dramacontroller.java
+++ demo/src/main/java/com/example/demo/chinadrama/Dramacontroller.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -32,5 +34,23 @@ public class Dramacontroller {
 	public String writeForm(Model model) {
 		model.addAttribute("menu", "drama");
 		return "drama/write";
+	}
+	
+	@PostMapping("/write")
+	public String writeProc(Drama drama) {
+		dramaService.insertDrama(drama);
+		return "redirect:/drama/list";
+	}
+	
+	@GetMapping("/detail/{no}")
+	public String detail(@PathVariable int no, String option, Model model) {
+		if(option == null || option.equals(""))
+		dramaService.increaseViewCount(no);
+		Drama drama = dramaService.getdrama(no);
+		model.addAttribute("drama",drama);
+		model.addAttribute("menu","drama");
+		return "drama/detail";
+		
+		
 	}
 }
